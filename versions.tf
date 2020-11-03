37272342deeab238a636a9914f0d5f6d4827261a
Sean Youngberg <syoungberg@paloaltonetworks.com>
2020-11-03 10:31:41 -0500

    single VPC example project using vpc, vpc_route, and vm_series modules

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
-
 terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+  required_version = ">=0.12.29, <0.14"
+}
+
+provider "aws" {
+  version = "~> 3"
+  region  = var.region
 }
